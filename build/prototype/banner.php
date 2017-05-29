f21cdaed340d9cc31b13f0ae825f10e3b1eabacc
Kevin Williams <kwilliams@priworks.com>
2017-05-29 12:16:18 -0400

    Alpesh initial sync

---
+++ build/prototype/banner.php
@@ -0,0 +1,16 @@
+<section id="banner" class="home-banner full-h-banner">
+    <img class="visible-xs" src="../images/home-banner-1.jpg" alt="">
+    <div class="container padding-none-xs">
+        <div class="banner-full-height">
+            <div class="banner-text">
+                <h1>Make your mark <span class="green">On</span> the world!</h1>
+                <p>generationOn provides programs, tools and resources to engage kiad teens in service and volunteering.</p>
+                <div class="btn-group">
+                    <a href="#" class="btn btn-purple">JOIN NOW</a>
+                    <a href="#" class="btn btn-purple btn-outline">LOGIN</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="#content" class="banner-scroll page-scroll"><img src="../images/icon-green-arrow-down.png" alt=""></a>
+</section>
---
+++ prototype/header.php
@@ -0,0 +1,32 @@
+<!-- Header Start -->
+<?php $file = basename($_SERVER['PHP_SELF']);?>
+<header id="header" class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            
+            <a class="navbar-brand" href="#"><img src="../images/logo.svg" alt=""></a>
+        </div>
+        <div class="navbar-header-right clearfix hidden-xs">
+            <ul class="clearfix">
+              <li><a href="#">Donate Now</a></li>
+              <li><a href="#">Join generationOn</a></li>
+            </ul>
+            <a href="#" class="btn btn-sm btn-white btn-outline">login</a>
+        </div>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="show">Menu</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <!-- Nav Start -->
+        <!--<nav id="nav">
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="<?php echo $file=="index.php"?"active":"";?>"><a href="index.php">Home</a></li>
+                    <li class="<?php echo $file=="about.php"?"active":"";?>"><a href="about.php">about</a></li>
+                </ul>
+            </div>
+        </nav>-->
+    </div>
+</header>
