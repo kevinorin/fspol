a7cfcee4156248bb5e04f09186563d63f62d9a4a
Kevin Williams <kwilliams@priworks.com>
2017-06-01 14:38:56 -0400

    Styling login page

--- build/banner-inner.php
+++ build/banner-inner.php
@@ -1,23 +1,28 @@
 <section id="banner" class="inner-banner full-h-banner">
-    <div class="container padding-none-xs">
-        <div class="banner-full-height">
-            <?php
-            if (stripos($_SERVER['REQUEST_URI'], 'dashboard.php')){
-                 echo '<div class="banner-text cleafix">
-                            <div class="banner-left">
-                                <h1>Welcome Joshua</h1>
-                            </div>
-                            <div class="banner-right">
-                                <a href="edit-profile.php" class="btn btn-white btn-outline">Edit profile</a>
-                            </div>
-                        </div>';
-            }
-            else if (stripos($_SERVER['REQUEST_URI'], 'track-service.php')){
-                 echo '<div class="banner-text track-banner cleafix text-center">
-                            <h1>Track Your Service</h1>
-                        </div>';
-                }
-            ?>
-        </div>
+  <div class="container padding-none-xs">
+    <div class="banner-full-height">
+      <?php
+        if (stripos($_SERVER['REQUEST_URI'], 'dashboard.php')){
+          echo '<div class="banner-text cleafix">
+            <div class="banner-left">
+              <h1>Welcome Joshua</h1>
+            </div>
+            <div class="banner-right">
+              <a href="edit-profile.php" class="btn btn-white btn-outline">Edit profile</a>
+            </div>
+          </div>';
+        }
+        else if (stripos($_SERVER['REQUEST_URI'], 'track-service.php')){
+          echo '<div class="banner-text track-banner cleafix text-center">
+            <h1>Track Your Service</h1>
+          </div>';
+        }
+        else if (stripos($_SERVER['REQUEST_URI'], 'login.php')){
+          echo '<div class="banner-text track-banner cleafix text-center">
+            <h1>Login to your account</h1>
+          </div>';
+        }
+      ?>
     </div>
+  </div>
 </section>
--- login.php
+++ login.php
@@ -8,16 +8,29 @@
     <?php include_once("header.php"); ?>
 
     <!-- Banner Start -->
-    <?php include_once("banner-login.php"); ?>
+    <?php include_once("banner-inner.php"); ?>
   
   
   <!-- Content Start -->
-  <div id="content" class="page-login">
-    <section class="section-block light-md-grey">
-      <div class="row no-col-space">
-        <div class="row-sm-height">
-          <div class="col-sm-8 col-sm-offset-2 .col-xs-12 col-middle">
-            <h2>You can login with your e-mail address<br />and password. Want to sign up?</h2>
+  <div id="content">
+    <section id="login" class="light-md-grey track-service-section track-service-1">
+      <div class="container">
+        <div class="track-service-center center-block white-bg panel-shadow">
+          <div class="track-service-inner">
+            <div class="track-steps text-center">
+              <h2>You can login with your e-mail address<br />and password. Want to sign up?</h2>
+
+              <!-- Login Form -->
+              <form id="login">
+                <div class="form-group">
+                  <label class="label-title">EMAIL</label>
+                  <input type="text" class="form-control" placeholder="EG. Animal Shelter Helpers!">
+                  <label class="label-title">PASSWORD</label>
+                  <input type="text" class="form-control" placeholder="EG. Animal Shelter Helpers!">
+                </div>
+              </form>
+              <!-- /Login Form -->
+            </div>
           </div>
         </div>
       </div>
