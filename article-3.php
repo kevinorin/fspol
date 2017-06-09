ab52f1f9efaf53ab43d4d323bec1fe7d1d598993
Alpesh <alpeshspce20@gmail.com>
2017-06-09 09:50:32 +0530

    Remaining pages + icons

---
+++ article-3.php
@@ -0,0 +1,128 @@
+<html>
+<head>
+  <?php include_once("head.php"); ?>
+  <title>Generation On</title>  
+</head>
+<body>
+	<div id="wrapper">
+	<?php include_once("header.php"); ?>
+	
+  <!-- Banner Start -->
+  <section id="banner" class="inner-banner breadcrumb-banner full-h-banner">
+    <div class="container padding-none-xs">
+        <div class="banner-full-height">
+            <div class="banner-text cleafix">
+                <h1>Dr. Martin Luther King Jr. Day</h1>
+                <ul class="breadcrumb">
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="#">Days of Service</a></li>
+                  <li class="active">Dr. Martin Luther King Jr. Day</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+  
+  <!-- Content Start -->
+  <div id="content">
+    <section class="article-section">
+        <div class="article-full">
+          <img src="assets/images/image-6.jpg" alt="">
+        </div>
+        <div class="article-content">
+          <div class="container">
+              <div class="row">
+                <div class="col-md-8 col-md-offset-2 col-sm-9 col-sm-offset-1">
+                  <div class="donate-left">
+                      <div class="article-title">
+                        <h2 class="h2 nevyblue">Praesent commodo cursus magna, vel scelerisque</h2>
+                        <h3 class="blue">July 25, 2016 | Authorname Lastname</h3>
+                        <a href="#" class="btn btn-grey btn-outline btn-radius">News</a>
+                      </div>
+                      <h3 class="nevyblue">Curabitur blandit tempus porttitor.</h3>
+                      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Vestibu-lum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+                      <p>Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus. Donec id elit non mi porta gravida at eget metus. Praesent com-modo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                      <div class="donate-right join-sidebar article-bottom">
+                        <h2 class="h2 nevyblue">Share this Article</h2>
+                        <div class="social-share">
+                          <ul class="clearfix">
+                            <li><a href="#"><em class="fa fa-facebook-square"></em></a></li>
+                            <li><a href="#"><em class="fa fa-twitter"></em></a></li>
+                            <li><a href="#"><em class="fa fa-youtube"></em></a></li>
+                            <li><a href="#"><em class="fa fa-pinterest"></em></a></li>
+                          </ul>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+    </section>
+    <section class="section-block our-program-section mddkgrey-bg">
+        <div class="container">
+          <div class="our-program-block feature-story custom-accordian">
+            <div class="section-title cust-acc-head text-center">
+              <h2 class="cust-acc-title skyblue">Featured Stories <em class="fa fa-angle-down visible-xs"></em></h2>
+            </div>
+            <div class="teaser-grid custom-accordian-body">
+              <div class="row">
+                <div class="col-sm-6">
+                  <div class="thumbnail blue-box">
+                    <div class="thumb">
+                      <img src="assets/images/image-2.jpg" alt="">
+                    </div>
+                    <div class="caption">
+                      <h3>Article Title</h3>
+                      <P>At nos hinc posthac, sitientis piros Afros. Etiam habebis sem dicantur magna mollis euismod.</P>
+                      <a href="#" class="btn btn-grey btn-outline btn-radius">News</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="thumbnail blue-box">
+                    <div class="thumb">
+                      <img src="assets/images/image-2.jpg" alt="">
+                    </div>
+                    <div class="caption">
+                      <h3>Another Article Title</h3>
+                      <P>At nos hinc posthac, sitientis piros Afros. Etiam habebis sem dicantur magna mollis euismod.</P>
+                      <a href="#" class="btn btn-grey btn-outline btn-radius">News</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="thumbnail blue-box">
+                    <div class="thumb">
+                      <img src="assets/images/image-2.jpg" alt="">
+                    </div>
+                    <div class="caption">
+                      <h3>Another Article Title</h3>
+                      <P>At nos hinc posthac, sitientis piros Afros. Etiam habebis sem dicantur magna mollis euismod.</P>
+                      <a href="#" class="btn btn-grey btn-outline btn-radius">News</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="thumbnail blue-box">
+                    <div class="thumb">
+                      <img src="assets/images/image-2.jpg" alt="">
+                    </div>
+                    <div class="caption">
+                      <h3>Another Article Title</h3>
+                      <P>At nos hinc posthac, sitientis piros Afros. Etiam habebis sem dicantur magna mollis euismod.</P>
+                      <a href="#" class="btn btn-grey btn-outline btn-radius">News</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+  </div>
+
+	<?php include_once("footer.php"); ?>
+</div><!--wrapper -->
+</body>
+</html>
